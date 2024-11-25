b176c4008d2eb93132f72439b7c3e7ac1127eee9
seongho9 <seongho9276@gmail.com>
2024-11-25 17:10:35 +0900

    [FEAT] main - test Config

--- rpi_cam_app/src/main.cpp
+++ rpi_cam_app/src/main.cpp
@@ -1,7 +1,17 @@
-#include "main.hpp"
+#include "config/ProgramConfig.hpp"
+#include "spdlog/spdlog.h"
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    config::ProgramConfig* config = config::ProgramConfig::get_instance();
+
+    config::EventConfig ev = config->event_config();
+
+
+    config::HttpConfig http = config->http_config();
+    config::VideoConfig vid = config->video_config();
+
+    spdlog::info("init");
+    
     return 0;
 }
