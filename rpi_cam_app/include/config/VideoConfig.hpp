7717826dd41207d0170e71c1ee8c3e519212c9e8
seongho9 <seongho9276@gmail.com>
2024-11-25 17:09:28 +0900

    [FEAT] VideoConfig

---
+++ rpi_cam_app/include/config/VideoConfig.hpp
@@ -0,0 +1,45 @@
+#ifndef _VIDEO_CONFIG
+#define _VIDEO_CONFIG
+
+#include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+#include "config/Config.hpp"
+
+namespace config
+{
+    class VideoConfig: public Config
+    {
+    private:
+        
+        boost::property_tree::ptree _props;
+        /// @brief 영상의 너비
+        int _width;
+        /// @brief 영상의 높이
+        int _height;
+        /// @brief frame rate로 "frame/second" 형식으로 기술
+        std::string _frame_rate;
+        /// @brief 영상 포맷
+        std::string _foramt;
+        /// @brief 영상을 저장하는 단위 시간(초)
+        int _split_time;
+        /// @brief 이벤트 발생시 보내는 영상의 길이(초)
+        int _duration;
+
+
+        int read_config() override;
+    public:
+        VideoConfig() = default;
+
+        int set_file(const std::string& json_file) override;
+
+        const int& width() const;
+        const int& height() const;
+        const std::string& frame_rate() const;
+        const std::string& foramt() const;
+        const int split_time() const;
+        const int duration() const;
+    };
+};
+#endif
