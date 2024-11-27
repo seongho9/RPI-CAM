25e1ffeaa1060215b78b4e383ff1a9e0ed3d4d9a
khy0e <dhy04029@gmail.com>
2024-11-27 17:16:45 +0900

    change VideoHandler class process_video function

--- rpi_cam_app/include/video/videoHandler.hpp
+++ rpi_cam_app/include/video/videoHandler.hpp
@@ -14,7 +14,7 @@ namespace video{
         VideoHandler();
         void get_video(std::string path, time_t timestamp);
         int process_video(time_t timestamp, uint64_t duration); 
-        void remove_video(uint64_t duration, std::string path); //timestamp를 넘겨받아야 할거 같은데..
+        void remove_video(int maintain_time, std::string path); //maintain_time를 넘겨받아야..
     };
 }
 #endif VIDEO_HANDLER_HPP
--- rpi_cam_app/src/video/videoHandler.cpp
+++ rpi_cam_app/src/video/videoHandler.cpp
@@ -1,9 +1,11 @@
 #include "video/videoHandler.hpp"
 #include "spdlog/spdlog.h"
 #include <filesystem>
+#include <algorithm>
 
 video::VideoHandler::VideoHandler(){
-
+    files.clear();
+    concat_file.clear();
 }
 void video::VideoHandler::get_video(std::string path, time_t timestamp){
 
@@ -12,6 +14,9 @@ void video::VideoHandler::get_video(std::string path, time_t timestamp){
             files.push_back(file.path().filename().string());
         }
     }
+    sort(files.begin(), files.end(), [](const string& a, const string& b) {
+        return stoi(a) < stoi(b);  
+    });
     
 }
 int video::VideoHandler::process_video(time_t timestamp, uint64_t duration)
@@ -31,19 +36,21 @@ int video::VideoHandler::process_video(time_t timestamp, uint64_t duration)
             concat_file.push_back(file);
         }
     }
-
+    //파이프라인 요소 구성
+    /*concat요소 : 여러 비디오 스트림을 하나로 결합하는 역할
+      qtmux: mp4 컨테이너를 생성하는 요소/ 비디오 스트림을 mp4파일로 패킹하는 역할
+      filesrc: location에 해당하는 파일을 읽어드리는 요소
+      qtdemux: mp4파일 포맷을 분해하는 요소 / mp4파일을 분석하여 포함되어 있는 비디오와 오디오 스트림 분리
+      h264parse: 비디오 스트림을 H.264 형식으로 파싱하는 요소
+      c. : concat의 첫 번째 입력포드에 데이터 전달*/
     std::stringstream pipeline;
+    pipeline << "concat name=c ! queue ! m.video_0 qtmux name=m ! filesink location=output_video.mp4";
 
-    for (size_t i = 0; i < concat_file.size(); ++i) {
-        pipeline << "filesrc location=" << concat_file[i] << " ! decodebin name=dec" << (i+1) 
-                 << " ! queue ! x264enc ! mp4mux name=mux4";
-        
-        if (i != concat_file.size() - 1) {
-            pipeline << " ! filesink location=result.mp4 ";
-        }
+    for (size_t i = 0; i < files.size(); ++i){
+        pipeline<< " filesrc location=" << concat_file[i] << " ! qtdemux ! h264parse ! c.";
     }
 
-    // GStreamer 파이프라인 실행
+    // 파이프라인 생성
     GstElement *pipeline_concat = gst_parse_launch(pipeline.str().c_str(), NULL);
     
     if (!pipeline_concat) {
@@ -61,8 +68,23 @@ int video::VideoHandler::process_video(time_t timestamp, uint64_t duration)
     return 0;
 
 }
-void video::VideoHandler::remove_video(uint64_t durtion, std::string path){
+void video::VideoHandler::remove_video(int maintain_time, std::string path){
     
+    int now = std::to_integer(time(nullptr));
     // 삭제하는 기준 정확하게 정해야
-    //std::filesystem::remove(path);
+    // duration을 받지 않고 유지 시간을 config 파일에서 수정 할 수 있게 해야
+    // 합쳐진 영상을 보냈다면, 서버가 합쳐진 영상을 잘 보냈다면
+    if(){
+        std::filesystem::remove(path+"/output_video.mp4");
+    } 
+
+    for (const auto& file : files) {
+        size_t pos = file.find('.');
+        time_t file_timestamp = std::stoi(file.substr(0, pos));
+        
+        // maintain_time을 분 단위라 가정 
+        if (file_timestamp < (now-(maintain_time*60))) {   
+            std::filesystem::remove(path+"/"+files[i]);
+        }
+    }
 }
