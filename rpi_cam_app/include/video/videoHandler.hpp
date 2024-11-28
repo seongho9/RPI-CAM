defaa67a6d82d4eb2545074df342641828beac2f
khy0e <dhy04029@gmail.com>
2024-11-28 17:16:50 +0900

    add set_filename(), modify all function in Videohandler class

--- rpi_cam_app/include/video/videoHandler.hpp
+++ rpi_cam_app/include/video/videoHandler.hpp
@@ -10,11 +10,13 @@ namespace video{
     class VideoHandler{
         std::vector<std::string> files;
         std::vector<std::string> concat_file;
-    public:
+        //friend Singleton<VideoHandler>; 
         VideoHandler();
-        void get_video(std::string path, time_t timestamp);
-        int process_video(time_t timestamp, uint64_t duration); 
-        void remove_video(int maintain_time, std::string path); //maintain_time를 넘겨받아야..
+    public:
+        int get_video(std::string eventId, time_t timestamp);
+        int process_video(time_t timestamp, uint64_t duration, std::string eventId); 
+        void set_filename(std::string path);
+        int remove_video(int maintain_time, std::string path); //maintain_time를 넘겨받아야..
     };
 }
 #endif VIDEO_HANDLER_HPP
--- rpi_cam_app/src/video/videoHandler.cpp
+++ rpi_cam_app/src/video/videoHandler.cpp
@@ -7,19 +7,32 @@ video::VideoHandler::VideoHandler(){
     files.clear();
     concat_file.clear();
 }
-void video::VideoHandler::get_video(std::string path, time_t timestamp){
+int video::VideoHandler::get_video(std::string eventId, time_t timestamp){
+    std::string filename = eventId+".mp4";
+    std::filesystem::path file_path(filename);
 
+    if (!file_path.has_filename()){
+        spdlog::warn("{} file doesnt exist..")
+        return -1;
+    }
+    
+    return 0
+
+}
+void video::VideoHandler::set_filename(std::string path){
     for(const auto& file: std::filesystem::directory_iterator(path)){
         if (file.is_regular_file() && file.path().extension() == ".mp4") {
             files.push_back(file.path().filename().string());
         }
     }
+
+    //timestamp 시간 순으로 정렬
     sort(files.begin(), files.end(), [](const string& a, const string& b) {
         return stoi(a) < stoi(b);  
     });
-    
 }
-int video::VideoHandler::process_video(time_t timestamp, uint64_t duration)
+
+int video::VideoHandler::process_video(time_t timestamp, uint64_t duration, std::string eventId)
 {
     //duration을 초단위로 해야..?
 
@@ -27,7 +40,7 @@ int video::VideoHandler::process_video(time_t timestamp, uint64_t duration)
     time_t start_time = timestamp - duration;
     time_t end_time = timestamp + duration;
 
-    // files에서 파일 이름 가져와서 
+    // files에서 파일 이름 가져와서 저장
     for (const auto& file : files) {
         size_t pos = file.find('.');
         time_t file_timestamp = std::stoi(file.substr(0, pos));
@@ -36,15 +49,10 @@ int video::VideoHandler::process_video(time_t timestamp, uint64_t duration)
             concat_file.push_back(file);
         }
     }
-    //파이프라인 요소 구성
-    /*concat요소 : 여러 비디오 스트림을 하나로 결합하는 역할
-      qtmux: mp4 컨테이너를 생성하는 요소/ 비디오 스트림을 mp4파일로 패킹하는 역할
-      filesrc: location에 해당하는 파일을 읽어드리는 요소
-      qtdemux: mp4파일 포맷을 분해하는 요소 / mp4파일을 분석하여 포함되어 있는 비디오와 오디오 스트림 분리
-      h264parse: 비디오 스트림을 H.264 형식으로 파싱하는 요소
-      c. : concat의 첫 번째 입력포드에 데이터 전달*/
+
+    // 파이프라인 구성
     std::stringstream pipeline;
-    pipeline << "concat name=c ! queue ! m.video_0 qtmux name=m ! filesink location=output_video.mp4";
+    pipeline << "concat name=c ! queue ! m.video_0 qtmux name=m ! filesink location=/event/video" << timestamp << ".mp4";
 
     for (size_t i = 0; i < files.size(); ++i){
         pipeline<< " filesrc location=" << concat_file[i] << " ! qtdemux ! h264parse ! c.";
@@ -68,23 +76,22 @@ int video::VideoHandler::process_video(time_t timestamp, uint64_t duration)
     return 0;
 
 }
-void video::VideoHandler::remove_video(int maintain_time, std::string path){
+int video::VideoHandler::remove_video(int maintain_time, std::string path){
     
     int now = std::to_integer(time(nullptr));
     // 삭제하는 기준 정확하게 정해야
     // duration을 받지 않고 유지 시간을 config 파일에서 수정 할 수 있게 해야
-    // 합쳐진 영상을 보냈다면, 서버가 합쳐진 영상을 잘 보냈다면
-    if(){
-        std::filesystem::remove(path+"/output_video.mp4");
-    } 
+    // 합쳐진 영상을 서버로 보내면 /event/done으로 옮겨서 해당 파일에 있는 영상 삭제
 
     for (const auto& file : files) {
         size_t pos = file.find('.');
         time_t file_timestamp = std::stoi(file.substr(0, pos));
         
         // maintain_time을 분 단위라 가정 
-        if (file_timestamp < (now-(maintain_time*60))) {   
-            std::filesystem::remove(path+"/"+files[i]);
+        if (file_timestamp < (now-(maintain_time*60))) {  
+            std::filesystem::remove(path+"/"+file);
+            spdlog::info("remove {} file successfully", file);
+            files.erase(file);
         }
     }
 }
