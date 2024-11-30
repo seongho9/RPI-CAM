91f91709e999b7e4e78c9a682ab5f05fb7d8b04c
seongho9 <seongho9276@gmail.com>
2024-11-30 23:42:01 +0900

    [FEAT] VideoQueue - insert_event

--- rpi_cam_app/src/camera_device/VideoQueue.cpp
+++ rpi_cam_app/src/camera_device/VideoQueue.cpp
@@ -23,7 +23,27 @@ VideoQueue::VideoQueue()
 
 int VideoQueue::insert_event(std::string name, ThreadInfo* thread_info)
 {
-    return 0;
+    _thread_info_mutex.lock();
+
+    auto item = _thread_info.find(name);
+
+    if(item != _thread_info.end()) {
+        spdlog::error("Event {} was created", name);
+        
+        _thread_info_mutex.unlock();
+        return 1;
+    }
+    else {
+        // push pop 연산시 연산강도 경감을 위함
+        //  ex. 60 / 7 = 8 -> 현 프레임에서 8의 배수 부분만 처리(8,16,24,32,40,48,56)
+        thread_info->_fps = (_device_fps / thread_info->_fps);
+
+        _thread_info.emplace(name, thread_info);
+        spdlog::info("Event {} has been inserted into the VideoQueue", name);
+
+        _thread_info_mutex.unlock();
+        return 0;
+    }
 }
 
 int VideoQueue::remove_event(std::string name)
