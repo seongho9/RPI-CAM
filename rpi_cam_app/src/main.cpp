37c9f5f497cbbd47935983fa82dfa7230b811350
khy0e <dhy04029@gmail.com>
2024-12-04 11:34:39 +0900

    [FIX] remove overlap pipeline

--- rpi_cam_app/src/main.cpp
+++ rpi_cam_app/src/main.cpp
@@ -9,11 +9,11 @@ int main(int argc, char const *argv[])
 {
     config::ProgramConfig* config = config::ProgramConfig::get_instance();
 
-    config::EventConfig ev = config->event_config();
-    spdlog::info("event names ");
-    for (auto item : ev.event_name()){
-        spdlog::info("{} {} fps", item, ev.event_fps(item));
-    }
+    // config::EventConfig ev = config->event_config();
+    // spdlog::info("event names ");
+    // for (auto item : ev.event_name()){
+    //     spdlog::info("{} {} fps", item, ev.event_fps(item));
+    // }
 
 
     const config::HttpConfig* http = config->http_config();
@@ -42,32 +42,17 @@ int main(int argc, char const *argv[])
 
     video::VideoInitializer* video_initializer = video::VideoInitializer::get_instance();
     video::VideoHandler* video_handler = video::VideoHandler::get_instance();
+    
     video_initializer->init();
     if (video_initializer->start() != 0) {
         spdlog::error("Failed to start video streaming server.");
         return -1;
     }
     
-    std::thread streaming([&](){
-        spdlog::info("Start Video streaming and saving");
-        video_initializer->start();
-    });
-    std::thread event([&]{
-        spdlog::info("Listening event.....");
-        video_initializer->set_event();
-    });
-    while(true){
-        video_initializer->event();
-        sleep(30);
-    }
-    int maintain_time = 10; //임시 설정. 10분
-    std::string path = "/home/pi/send"; //임시 설정. 완료된 영상을 서버에게 잘 보냈을 경우 파일이 있는 위치
-    std::thread remove_video([&]{
-        video_handler->remove_video(maintain_time, path);
-    });
-    streaming.join();
-    event.join();
-    remove_video.join();
+    // thread
+    // 이벤트가 발생하는지 기다리는 함수가 
+    // 이벤트 발생하면 video_initializer->set_event();
+    // video_initializer->event();
 
     // Stop video streaming and saving
     if (video_initializer->stop() != 0) {
