a0b69aef254d707551213863519c770e0817c2fc
khy0e <dhy04029@gmail.com>
2024-12-06 10:48:01 +0900

    [TEST] fix test code

--- rpi_cam_app/test/test_main.cpp
+++ rpi_cam_app/test/test_main.cpp
@@ -39,20 +39,26 @@ int main(int argc, char const *argv[])
     std::atomic<bool> running(true);
 
     std::thread video_event_thread([&](){
-        // 스트리밍 30초 후 이벤트가 발생한다고 가정
+        // 스트리밍 30초 후 1번 이벤트가 발생한다고 가정
         std::this_thread::sleep_for(std::chrono::seconds(30));
-        video_initializer->set_event();
+        int timestamp = 1738129293;  // 해당 timestamp 사용하려면 영상 파일 2개 필요(ex. 173812929.mp4, 173812930.mp4)
+        std::string eventId = "event1";
+        std::string path = "/home/pi/final5";
+        video_initializer->set_event(path, eventId, timestamp);
     });
 
+    //일정 시간 지나면 video 삭제 thread
+    /*
     int maintain_time = 10; //임시 설정. 10분
     std::string path = "/home/pi/final5"; //임시 설정. 완료된 영상을 서버에게 잘 보냈을 경우 파일이 있는 위치
 
-    // std::thread remove_video_thread([&](){
-    //     video_handler->remove_video(maintain_time, path);
-    // });
-    while(1);
+    std::thread remove_video_thread([&](){
+         video_handler->remove_video(maintain_time, path);
+    });*/
+
+    // 이벤트 처리
     while(running){
-        //이벤트 처리
+        //임시 값
         video_initializer->event();
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
